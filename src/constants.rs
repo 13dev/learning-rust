e1474a06591157b2ab5676f7af84464d2522adce
13dev <qwerty124563@gmail.com>
2020-11-15 23:28:54 +0000

    Add Constants

---
+++ src/constants.rs
@@ -0,0 +1,7 @@
+const MAXIMUM_NUMBER: u8 = 10;
+
+pub fn testing_constant() {
+    for i in 0..MAXIMUM_NUMBER {
+        println!("{}", i);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod loops;
 mod enums;
+mod constants;
 
 fn main() {
-    loops::infinite_loop();
-    loops::while_loop();
-    loops::for_loops();
+    //loops::infinite_loop();
+    //loops::while_loop();
+    //loops::for_loops();
 
-    enums::var_enum();
+    //enums::var_enum();
+
+    constants::testing_constant();
 
     println!("Hello, world!");
 }
