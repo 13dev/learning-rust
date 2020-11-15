58315b634d27fb70544dbae90fb563e861879987
13dev <qwerty124563@gmail.com>
2020-11-15 23:09:16 +0000

    Add for loops

--- src/loops.rs
+++ src/loops.rs
@@ -1,3 +1,5 @@
+use std::ops::Range;
+
 pub fn infinite_loop() {
     let mut counter: i32 = 0;
     loop {
@@ -9,4 +11,39 @@ pub fn infinite_loop() {
 
         println!("Looping... {}", counter);
     }
+}
+
+pub fn while_loop() {
+    let mut counter = 0;
+
+    while counter <= 10 {
+        counter += 1;
+
+        println!("While loop {}", counter);
+    }
+
+}
+
+pub fn for_loops() {
+    // normal for loop
+    for i in 1..5 {
+        println!("For loop {}", i);
+    }
+
+    //let range: Range<i32> = 0..10;
+    let range = 0..5;
+    for i in range {
+        println!("For loop with range {}", i);
+    }
+
+    // for loop with vectors
+    let my_vector = vec!["Maça", "Laranja", "Pero"];
+
+    // without the .iter(), we cannot access my_vector later
+    for piece in my_vector.iter() {
+        println!("Vector for loop: {}", piece);
+    }
+
+    println!("Accessing Laranja vector pos: {}", my_vector[1]);
+
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ mod loops;
 
 fn main() {
     loops::infinite_loop();
+    loops::while_loop();
+    loops::for_loops();
+
     println!("Hello, world!");
 }
